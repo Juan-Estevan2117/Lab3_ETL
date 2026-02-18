6b2fb0f2495475d48dfa21d58dce41ab864ee89e
juanesklk <juan.viera@uao.edu.co>
2026-02-18 00:28:41 -0500

    Se añade archivo extraction.py

---
+++ etl/extract.py
@@ -0,0 +1,19 @@
+# Testing extract.py
+def extract_data(raw_data_path):
+    """
+    Extrae los datos desde los archivos CSV de la capa cruda.
+    """
+    print("Iniciando proceso de Extracción (Extract)...")
+    
+    try:
+        df_channels = pd.read_csv(os.path.join(raw_data_path, 'channels.csv'))
+        df_customers = pd.read_csv(os.path.join(raw_data_path, 'customers.csv'))
+        df_products = pd.read_csv(os.path.join(raw_data_path, 'products.csv'))
+        df_sales = pd.read_csv(os.path.join(raw_data_path, 'sales.csv'))
+        
+        print("✅ Extracción completada con éxito.")
+        return df_channels, df_customers, df_products, df_sales
+        
+    except Exception as e:
+        print(f"Error durante la extracción: {e}")
+        return None, None, None, None
